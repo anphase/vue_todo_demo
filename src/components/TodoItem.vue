1ff2a3dc520f120a2440fb238d5400355a0511d4
Panashe Desktop VMWare <panashe@outlook.com>
2019-07-16 10:07:48 +1200

    Completed todo app demo

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,48 @@
+<template>
+<!-- binding the is-complete css on the condition that the todo is completed -->
+    <div class="todo-item" v-bind:class="{'is-complete':todo.completed}">
+        <p>
+            <!-- Add event and define in script methods -->
+            <input type="checkbox" v-on:change="markComplete">
+            {{todo.title}}
+            <!-- Emit the delete event to pass it up to the Todos object -->
+            <button @click="$emit('del-todo', todo.id)" class="del">x</button>
+            </p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "TodoItem",
+    props: ["todo"],
+    methods: {
+        markComplete() {
+            // Toggle
+            this.todo.completed = !this.todo.completed;
+        }
+    }
+}
+</script>
+
+
+<style scoped>
+    .todo-item {
+        background: #f4f4f4;
+        padding: 10px;
+        border-bottom: 1px #ccc dotted;
+    }
+
+    .is-complete {
+        text-decoration: line-through;
+    }
+
+    .del {
+        background: #ff0000;
+        color: #fff;
+        border: none;
+        padding: 5px 9px;
+        border-radius: 50%;
+        cursor: pointer;
+        float: right;
+    }
+</style>
---
+++ src/views/About.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <p>This is a gift, that comes with a price</p>
+  </div>
+</template>
